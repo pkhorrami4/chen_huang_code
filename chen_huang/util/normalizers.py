f4a21c7ddfbf99158869e711b854e725767abcc7
Pooya Khorrami <pkhorrami4@gmail.com>
2016-10-06 14:39:59 -0500

    Add simple normalizer for landmark and landmark_diff features.

---
+++ chen_huang/util/normalizers.py
@@ -0,0 +1,30 @@
+import os
+import numpy
+
+
+class LandmarkFeatureNormalizer(object):
+    def __init__(self, npy_file_path, fold_type, fold):
+        assert fold_type in ['subj_dep', 'subj_ind'], 'fold_type must be subj_dep or subj_ind'
+
+        if fold_type == 'subj_dep':
+            assert fold in range(3), 'fold %d does not exist' % fold
+        else:
+            assert fold in range(20), 'fold %d does not exist' % fold
+
+        self.npy_file_path = npy_file_path
+        self.fold_type = fold_type
+        self.fold = fold
+
+        self.npy_filename = os.path.join(npy_file_path, 
+                                         'global_stats_'+fold_type+'.npy')
+        
+        global_stats_dict = numpy.load(self.npy_filename)
+        global_stats_dict = global_stats_dict[()]
+
+        self.mean_vector = global_stats_dict[fold]['mean']
+        self.std_vector = global_stats_dict[fold]['std']
+
+    def run(self, x_batch):
+        x_batch_norm = (x_batch - self.mean_vector[None, None, :])
+        x_batch_norm /= (self.std_vector[None, None, :] + 1e-6)
+        return x_batch_norm
